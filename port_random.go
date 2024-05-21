c26d2659194dbbff989f729f13cfaf7d2f208260
joe-at-startupmedia <joseph.persie@startupmedia.com>
2024-05-21 05:55:19 +0000

    Adds a mutex lock

--- port_random.go
+++ port_random.go
@@ -4,23 +4,37 @@ package gipc
 
 import (
 	"math/rand"
+	"sync"
 )
 
+var mutex = &sync.Mutex{}
 var ports = make(map[string]int, 200)
 
 func randRange(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func getPortByName(name string) int {
+	mutex.Lock()
+	port := ports[name]
+	mutex.Unlock()
+	return port
+}
+
+func setPortByName(name string, port int) {
+	mutex.Lock()
+	ports[name] = port
+	mutex.Unlock()
+}
+
 func GetPort(name string) int {
-	var port int
-	if ports[name] > 0 {
-		port = ports[name]
+	port := getPortByName(name)
+	if port > 0 {
+		return port
 	} else {
 		port = GetDefaultPort()
 		port = randRange(port, port+200)
-		ports[name] = port
+		setPortByName(name, port)
+		return port
 	}
-
-	return port
 }
